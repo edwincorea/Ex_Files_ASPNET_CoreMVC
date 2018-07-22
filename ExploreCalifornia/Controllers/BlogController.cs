9e0ce2fc56a7a5ae251044a8a34b1a1d3b61c80c
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-07-22 17:14:23 -0600

    Customize your application's URLs.

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -2,19 +2,21 @@
 
 namespace ExploreCalifornia.Controllers
 {
+    [Route("blog")]
     public class BlogController : Controller
     {
+        [Route("")]
         public IActionResult Index()
         {
             return new ContentResult { Content = "Blog posts" };
         }
 
-        public IActionResult Post(int? id)
+        [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year, int month, string key)
         {
-            if (id == null)
-                return new ContentResult { Content = "null" };
-
-            return new ContentResult { Content = id.ToString() };
+            return new ContentResult {
+                Content = string.Format("Year: {0}; Month: {1}; Key: {2}", year, month, key)
+            };
         }
     }
 }
