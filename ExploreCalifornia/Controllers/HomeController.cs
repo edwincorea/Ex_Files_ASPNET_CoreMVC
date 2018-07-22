596e1e4e033cb04d1225749f8ace8a657fb6c9bc
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-07-22 17:26:38 -0600

    Render HTML with Razor.

--- ExploreCalifornia/Controllers/HomeController.cs
+++ ExploreCalifornia/Controllers/HomeController.cs
@@ -2,13 +2,11 @@
 
 namespace ExploreCalifornia.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        //public string Index()
         public IActionResult Index()
         {
-            // return "Hello, ASP.Net Core MVC!";
-            return new ContentResult { Content = "Hello, ASP.Net Core MVC!" };
+            return View();
         }
     }
 }
