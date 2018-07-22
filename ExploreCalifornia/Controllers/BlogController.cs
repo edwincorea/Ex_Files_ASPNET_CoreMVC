38c3099f17e329569fef2a143006ee075e4dc7c3
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-07-22 16:47:21 -0600

    Handle requests with controllers.

---
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "Blog posts" };
+        }
+    }
+}
---
+++ ExploreCalifornia/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.Controllers
+{
+    public class HomeController
+    {
+        //public string Index()
+        public IActionResult Index()
+        {
+            // return "Hello, ASP.Net Core MVC!";
+            return new ContentResult { Content = "Hello, ASP.Net Core MVC!" };
+        }
+    }
+}
