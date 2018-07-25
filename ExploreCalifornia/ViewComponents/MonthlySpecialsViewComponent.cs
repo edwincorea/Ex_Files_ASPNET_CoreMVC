75cec33fdf4585a3e80d04c44b2eefd2cf70438b
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-07-25 15:36:01 -0600

    Reuse application features with view components, part 2.

--- ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
+++ ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExploreCalifornia.ViewComponents
 {
     [ViewComponent]    
     public class MonthlySpecialsViewComponent : ViewComponent
     {
+        private readonly SpecialsDataContext _specials;
+
+        public MonthlySpecialsViewComponent(SpecialsDataContext specials)
+        {
+            this._specials = specials;
+        }
+
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var specials = this._specials.GetMonthlySpecials();
+            return View(specials);
         }
     }
 }
