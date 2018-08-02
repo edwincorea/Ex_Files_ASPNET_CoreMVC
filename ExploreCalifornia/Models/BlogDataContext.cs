0848f33c4d969306f1747957e827aa82bbbfc472
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-08-02 16:48:17 -0600

    Expose data through a web API.

--- ExploreCalifornia/Models/BlogDataContext.cs
+++ ExploreCalifornia/Models/BlogDataContext.cs
@@ -4,8 +4,9 @@ namespace ExploreCalifornia.Models
 {
     public class BlogDataContext : DbContext
     {
+        public DbSet<Comment> Comments { get; set; }
         public DbSet<Post> Posts { get; set; }
-
+        
         public BlogDataContext(DbContextOptions<BlogDataContext> options) 
             : base(options)
         {
--- ExploreCalifornia/api/CommentsController.cs
+++ ExploreCalifornia/api/CommentsController.cs
@@ -1,41 +1,94 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExploreCalifornia.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExploreCalifornia.api
 {
-    [Route("api/[controller]")]
+    [Route("api/posts/{postKey}/comments")]
     public class CommentsController : Controller
     {
-        // GET: api/<controller>
+        private readonly BlogDataContext _db;
+
+        public CommentsController(BlogDataContext db)
+        {
+            this._db = db;
+        }
+
+        // GET: api/posts/{postKey}/comments
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get(string postKey)
         {
-            return new string[] { "value1", "value2" };
+            return Ok(_db.Comments.
+                Where(c => c.Post.Key == postKey).
+                ToList());
         }
 
-        // GET api/<controller>/5
+        // GET api/posts/{postKey}/comments/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(string postKey, int id)
         {
-            return "value";
+            var comment = _db.Comments.
+                FirstOrDefault(c => c.Post.Key == postKey && c.Id == id);
+
+            if (comment == null)
+                return NotFound();
+
+            return Ok(comment);
         }
 
-        // POST api/<controller>
+        // POST api/posts/{postKey}
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post(string postKey, [FromBody]Comment comment)
         {
+            var post = _db.Posts.FirstOrDefault(p => p.Key == postKey);
+
+            if (post == null)
+                return NotFound();
+
+            comment.Post = post;
+            comment.Posted = DateTime.Now;
+            comment.Author = User.Identity.Name;
+
+            _db.Comments.Add(comment);
+            _db.SaveChanges();
+
+            return Ok(comment);
         }
 
-        // PUT api/<controller>/5
+        // PUT api/posts/{postKey}/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(string postKey, int id, [FromBody]Comment comment)
         {
+            var commentDb = _db.Comments.
+                FirstOrDefault(c => c.Post.Key == postKey && c.Id == id);
+
+            if (commentDb == null)
+                return NotFound();
+
+            commentDb.Body = comment.Body;
+
+            _db.Comments.Update(commentDb);
+            _db.SaveChanges();
+
+            return NoContent();
         }
 
-        // DELETE api/<controller>/5
+        // DELETE api/posts/{postKey}/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(string postKey, int id)
         {
+            var commentDb = _db.Comments.
+                FirstOrDefault(c => c.Post.Key == postKey && c.Id == id);
+
+            if (commentDb == null)
+                return NotFound();
+
+            _db.Comments.Remove(commentDb);
+            _db.SaveChanges();
+
+            return NoContent();
         }
     }
 }
