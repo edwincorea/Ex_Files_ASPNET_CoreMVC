29b68a21f90381991fba08f69b0e755b10fd68f7
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-08-02 18:17:57 -0600

    Add identity services to your application.

---
+++ ExploreCalifornia/Models/IdentityDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExploreCalifornia.Models
+{
+    public class IdentityDataContext : IdentityDbContext<IdentityUser>
+    {
+        public IdentityDataContext(DbContextOptions<IdentityDataContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -1,8 +1,7 @@
 ﻿using ExploreCalifornia.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -47,6 +46,15 @@ namespace ExploreCalifornia
                 options.UseSqlServer(connectionString);
             });
 
+            services.AddDbContext<IdentityDataContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("IdentityDataContext");
+                options.UseSqlServer(connectionString);
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDataContext>();
+
             services.AddMvc();
         }
 
@@ -72,6 +80,8 @@ namespace ExploreCalifornia
 
                 await next();
             });
+
+            app.UseAuthentication();
 
             app.UseMvc(routes => {
                 routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
